e3340e370266f15403309f1a89f69c8bab5e0bbc
Daekwang.Kim <daekwang.kim@onyxinsight.com>
2019-08-27 10:35:10 +0900

    Chapter 2-5

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,4 +13,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //aaaaaaaaaaaaaaa
     }
+
+//    fun onClicked(v : View) {
+//        var nextIntetn = Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1234-5678"));
+//        startActivity(nextIntetn);
+//    }
 }
